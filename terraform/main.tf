0a293e28882003b7e8de717beed24a1c236f2a10
rsargunar <62004678+rsargunar@users.noreply.github.com>
2025-11-24 21:26:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,9 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
 
 resource "azurerm_subnet" "subnet" {
