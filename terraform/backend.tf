70883ba67e70d1531c86f2e8a7c57812c1ae35f3
Robinson <g.robinsonisrael@gmail.com>
2025-11-27 08:05:51 +0530

    updated terraform-d backend

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tfstate-rg"
+        storage_account_name = "tfstatestorage100"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
